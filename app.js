4955d7096ce50e96703a24e72836cd24195124aa
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-01-14 11:47:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+//For Of Looops------------- 
+
+
+// let str = "Abdul Majeed";
+
+// let size = 0;
+// for(i of str) {
+//     console.log("i =", i);
+//     size++;
+// }
+
+// console.log("size = ", size)
+
+
+// For in loops -------------
+
+// let studet ={
+//     name: "Abdul Majeed", 
+//     rollNumber: 68, 
+//     CGPA: 2.3, 
+//     isPass: true, 
+// };
+
+// for (let key in studet){
+//     console.log("key=", key , "val=", studet[key]);
+// }
